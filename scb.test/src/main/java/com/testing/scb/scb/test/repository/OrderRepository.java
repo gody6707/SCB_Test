dfa686ec53c98268e27e41a0273721609090253e
gody6707 <got_za017@hotmail.com>
2018-10-02 14:10:06 +0700

    prasit

--- scb.test/src/main/java/com/testing/scb/scb/test/repository/OrderRepository.java
+++ scb.test/src/main/java/com/testing/scb/scb/test/repository/OrderRepository.java
@@ -2,10 +2,10 @@ package com.testing.scb.scb.test.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.stereotype.Repository;
 import com.testing.scb.scb.test.model.OrdersModel;
 
-
+@Repository
 public interface OrderRepository extends JpaRepository<OrdersModel, Object>  {
 	
 	@Query(value = "Select price from order Where users = ?1  AND orderId = ?2", nativeQuery = true)
